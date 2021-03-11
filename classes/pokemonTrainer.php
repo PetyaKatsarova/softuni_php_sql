935bd8550042bde23b657514b06c5907cc52b1c3
PetyaKatsarova <pskpetya@yahoo.com>
2021-03-11 10:10:18 +0100

    more classes code

--- classes/pokemonTrainer.php
+++ classes/pokemonTrainer.php
@@ -13,6 +13,10 @@ class Trainer{
         $this->badges = 0;
         $this->pokemons = [];
     }
+    public function getBadges(){
+        return $this->badges;
+    }
+
     public function catchPokemon(Pokemon $pokemon):void{
         $this->pokemons[$pokemon->getElement()][] = $pokemon;
     }
@@ -21,6 +25,24 @@ class Trainer{
     }
     public function hasPokemonsByElement(string $element):bool{
         return key_exists($element,$this->pokemons)&&count($this->pokemons[$element]) > 0;
+    }
+    public function hitPokemons(int $dmg): void{
+        foreach($this->pokemons as $type=>$pokemonsByType){
+            foreach($pokemonsByType as $key=>$pokemon){
+                $pokemon->hit($dmg);
+                if(!$pokemon->isAlive()){
+                    unset($this->pokemons[$type][$key]);
+                }
+            }
+        }
+    }
+    public function __toString()
+    {
+        $pokemonCount = 0;
+        foreach($this->pokemons as $pokemonsByType){
+            $pokemonCount += count($pokemonsByType);
+        }
+        return $this->name . ' ' . $this->badges . ' ' . $pokemonCount;
     }
 }
 
@@ -53,6 +75,48 @@ class Pokemon{
         $this->health -= max(0, $dmg);
     }
 }
+
+$trainers = [];
+$line = readline();
+while($line != 'Tournament'){
+    list($trainerName, $pokemonName, $element, $health) = explode(' ', $line);
+    if(!key_exists($trainerName, $trainers)){
+        $trainers[$trainerName] = new Trainer($trainerName);
+    }
+    $trainer = $trainers[$trainerName];
+    $trainer->catchPokemon(new Pokemon($pokemonName, $element, $health));
+
+    $line = readline();
+}
+
+$Line = readline();
+while($line != 'End'){
+    foreach($trainers as $trainer){
+        if($trainer->hasPokemonsByElement($line)){
+            $trainer->receiveBadge();
+        }else{
+            $trainer->hitPokemons(dmg:10);
+        }
+    }
+
+    $line = readline();
+}
+
+uksort($trainers, function($k1,$k2) use ($trainers){
+    $trainer1 = $trainers[$k1];
+    $trainer2 = $trainers[$k2];
+    return $trainer2->getBadges() <=> $trainer1->getBadges();
+});
+echo implode(PHP_EOL, $trainers);
+
+$arr = ['Stamat', 'Blast', 'Water', 18,
+'Nasko', 'Pikachu', 'Electricity', 22,
+'Jicat', 'Sth', 'Psychic', 90,
+'Tournament', 'Fire', 'Electricity', 'Fire', 'End'];
+// Nasko 1 1
+//Stamat 0 0
+//Jicat 0 1
+
 // TrainerName PokemonName PokemonElement PokemonHealth”, after command 'tournament': 1 of 3 els: 'fire, water, electricity till 'End': check if at least 1 trainer has a pokemon with this element
 
 // Define a class
