e1a2c18341875275008d1bb4cfeb16f37a76ee1c
PetyaKatsarova <pskpetya@yahoo.com>
2021-03-12 17:25:51 +0100

    more encapsulation code

---
+++ encapsulation_inheritance/ShoppingSpree/Product.php
@@ -0,0 +1,30 @@
+<?php
+class Product{
+    private $name;
+    /** @var float*/
+    private $cost;
+    public function __construct()
+    { 
+        $this->setName($this->name);
+        $this->setCost($this->cost);
+    }
+    public function getName():string{
+        return $this->name;
+    }
+    private function setName($n):void{
+        $this->validatePositiveNum(strlen($n), 'Name');
+        $this->name = $n;
+    }
+    public function getCost():float{
+        return $this->cost;
+    }
+    private function setCost($c){
+        $this->validatePositiveNum($c, 'Cost');
+        $this->cost = $c;
+    }
+    private function validatePositiveNum($val, $param):void{
+        if($val <= 0){
+            throw new Exception("$param can't be negative or empty.");
+        }
+    }
+}
--- encapsulation_inheritance/box.php
+++ encapsulation_inheritance/box.php
@@ -1,39 +1,73 @@
 <?php 
 class Box{
+    /** 
+    * @var float
+    */
     private $length; 
     private $width; 
     private $height;
 
-    public function __construct($l, $w, $h)
+    /*
+    @param float $length
+    @param float $width
+    @param float $height
+    */
+    public function __construct(float $l, float $w, float $h)
     {
+        $this->setLength($l);
+        $this->setWidht($w);
+        $this->setHeight($h);;
+    }
+
+    // use if you need validation
+    private function setLength($l):void{
+        $this->validateInput($l, "Length");
         $this->length = $l;
+    }
+    private function setWidht($w):void{
+        $this->validateInput($w, 'Width');
         $this->width = $w;
+    }
+    private function setHeight($h):void{
+        $this->validateInput($h, 'Height');
         $this->height = $h;
     }
-    public function printSurfaceArea(){
+    public function getHeight():float{
+        return $this->height;
+    }
+    public function getWidth():float{
+        return $this->width;
+    }
+    public function getLength():float{
+        return $this->length;
+    }
+    private function getVolume():float{
+        return $this->getLength()*$this->getWidth()*$this->getHeight();
+    }
+    private function getSurfaceArea():float{
         //  formula surfaceArea = 2lw + 2lh + 2wh
-        $surfaceArea = 2*($this->length)*($this->width) + 2*($this->length)*($this->height) + 2*($this->height)*($this->width);
-       echo 'Surface Area - ' . round($surfaceArea, 2);
+        return 2*($this->getLength() * $this->getWidth()) + 2*($this->getLength()*$this->getHeight()) + 2*($this->getHeight() * $this->getWidth());
     }
-    // = 2lh + 2wh
-    public function printLateralSurface(){
+    private function validateInput(float $val, string $param):void{
+        if($val <= 0){
+            throw new Exception("$param can't be negative.");
+        }
+    }
 
-    }
-    // = lwh
-    public function printVolume(){
-        $volume = $this->length * $this->width * $this->height;
-       echo 'Volume - ' . round($volume, 2);
+
+    public function __toString()
+    {
+     // $volume = number_format($volume, decimals:2, dec_point:'.', thousands_sep:'');
+       $volume = number_format($this->getVolume(), 2, '.', '');
+       $surfaceArea = number_format($this->getSurfaceArea(), 2, '.', '');
+       return "Surface Area - {$surfaceArea} <br/> Volume - {$volume}";
     }
 }
 $box1 = new Box(2,3,4);
-echo $box1->printSurfaceArea() . "<br>";// surfacearea - 52.00
-echo $box1->printVolume();//volum - 24.00
+echo $box1;//52.00 24.00
 echo '<br>-------------------<br>';
 $box2 = new Box(1.3, 1, 6);
-echo $box2->printSurfaceArea() . "<br>";// surfacearea - 30.20
-echo $box2->printVolume();// volume - 7.80
-
-
+echo $box2;// 30.20 7.80
 
 // You are given a geometric figure box with parameters length, width and height. Model a class Box that that can be instantiated by the same three parameters. Expose to the outside world only methods for its surface area, lateral surface area and its volume (formulas: http://www.mathwords.com/r/rectangular_parallelepiped.htm).The lateral surface area of a three-dimensional object is the surface area of the object minus the area of its bases.On the first three lines you will get the length, width and height. Print three lines with the surface area, lateral
 // surface area and the volume of the box:
