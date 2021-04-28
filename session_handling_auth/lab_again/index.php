a49179ebc31794d85bac44fe89a2c8b9d640328f
PetyaKatsarova <pskpetya@yahoo.com>
2021-04-28 10:21:47 +0200

    creating new simple app with login/logout/profile using complex division of classes, interfaces in many folders and interacting with mysql db

---
+++ session_handling_auth/lab_again/index.php
@@ -0,0 +1,5 @@
+<?php
+// require_once 'common.php';
+// $userService = new \App\Service\UserService(new \App\Repository\UserRepository($db));
+// $homeHandler = new \App\Http\HomeHttpHandler($template);
+// $homeHandler->index($userService);
---
+++ session_handling_auth/lab_again/proba.php
@@ -0,0 +1,41 @@
+<?php
+class UserDTO{
+    private $id;
+    private $username;
+    private $PASSWORD;
+    private $profile_picture_url;
+    private $born_on;
+
+    public function getId(){
+        return $this->id;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+    public function getUsername(){
+        return $this->username;
+    }
+    public function setUsername($username){
+        $this->username = $username;
+    }
+    public function getPassword(){
+        return $this->PASSWORD;
+    }
+    public function setPasssword($PASSWORD){
+        $this->PASSWORD = $PASSWORD;
+    }
+}
+
+$pdo = new PDO(dsn:"mysql:host=localhost:3306;dbname=test",username:"root");
+$stmt = $pdo->prepare("SELECT * FROM users2");
+$stmt->execute();
+// to use USERDTO: INSTEAD OF fetchAll(..) use FETCHOBJECT(UserDTO::class)
+$users2 = $stmt->fetchObject(UserDTO::class);
+
+// WE WORK WITH CLASS: NEED TO CALL THE METHODS!!
+while($row = $stmt->fetchObject(UserDTO::class)){
+    echo $row->getUsername() . " // " . $row->getId() . "</br>";
+    // print_r($row);
+}
+
+
